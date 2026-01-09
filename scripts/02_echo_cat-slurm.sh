6421c5becf7daa7a5bc2b00643b060150ac9b456
David Stanton <stantondw@cardiff.ac.uk>
2026-01-09 13:31:50 +0000

    Update 02_echo_cat-slurm.sh

--- scripts/02_echo_cat-slurm.sh
+++ scripts/02_echo_cat-slurm.sh
@@ -9,5 +9,10 @@
 
 DATA="/mnt/scratch/FYPs_2526/"
 
+### This is a "slurm script" - gomphus will recognise it as such due to the header (note the difference to the last script)
+### Slurm is a job scheduler - it uses a queuing system to manage jobs on the cluster
+### Details of this are given in "04 - Editing and submitting scripts"
+### You submit these scripts to the queue by typing: sbatch script.sh
+
 echo "Hello, world!"
 cat ${DATA}Test_file.txt
