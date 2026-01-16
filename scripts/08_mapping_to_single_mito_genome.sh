32f3ff27e68ec61ece61c1db036c6b7ee9baf172
David Stanton <stantondw@cardiff.ac.uk>
2026-01-16 13:51:59 +0000

    Update 08_mapping_to_single_mito_genome.sh

--- scripts/08_mapping_to_single_mito_genome.sh
+++ scripts/08_mapping_to_single_mito_genome.sh
@@ -37,19 +37,4 @@ REF="/mnt/scratch/[USERNAME]/[YOUR_MITO_GENOME].fasta"
 ## 1. Submit them all in parallel (as per previous scripts)
 ## 2. Loop through your files with a simple "bash loop" and output the results into a single output file
 
-samtools view -q 20 ${DIR}[YOUR_FILE].bam | awk '{print $3}' | sort | uniq -c | sort -k1,1 -nr > [YOUR_FILE]_MultiMito_q20.out
-
-## NOTES
-# "samtools" program for viewing/summarising bam/sam files
-# "-q 20 " this is a "flag" to modify what samtools does - Q: What does this do??
-# "|" this symbol is a "pipe" this means that the output of samtools is being redirected into another command (awk)
-# "awk '{print $3}' " awk is a basic program for viewing editing files, "print $3" means only print the 3rd column
-# "sort" sort the output
-# "uniq -c" only give unique values, and count (-c) each occurrance
-# "sort -k1,1 -nr" sort again by column 1 (-k1,1), as reverse numeric (-nr)
-# ">" direct output into a file (called "[YOUR_FILE]_MultiMito_q20.out")
-
-## QUESTIONS
-# What does this output tell you?
-# What is the difference between the two versions (-q20 used/not used)?
-# What are the limitations of this method?
+bwa aln -l 16500 -n 0.01 -o 2 -t 1 ${REF} ${DIR}[YOUR_SAMPLE1]_merged.fq.gz | bwa samse ${REF} - ${DIR}/[YOUR_SAMPLE1]_merged.fq.gz | samtools sort - > ${DIR}/[YOUR_SAMPLE1].single.mito.bam
